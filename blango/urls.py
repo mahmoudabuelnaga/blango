efda9d4386909db54e1c081804e6d869b22bf210
mahmoud aboelnaga <mahmoudaboelnaga392@gmail.com>
2022-04-20 03:06:16 +0000

    Finish django configurations

--- blango/urls.py
+++ blango/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import blog.views
+from django.conf import settings
+
+print(f"Time Zone: {settings.TIME_ZONE}")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
-    path('post/<int:pk>', blog.views.post_detail, name="post_detail")
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
+
 ]
